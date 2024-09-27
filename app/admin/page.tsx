4032e373977c071d0f7c61b84370034342569d7f
Leon Staufer <leon@staufer.me>
2024-09-27 17:15:56 +0200

    update ui for admin songs

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,27 +1,30 @@
-'use client';
+"use client";
 
-import { Button } from '@/components/ui/button';
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardFooter,
   CardHeader,
   CardTitle,
-} from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { createRoom, startRound } from '@/lib/rooms';
-import { useSongsOfRound } from '@/lib/songs';
-import { state } from '@/lib/state';
-import { useState } from 'react';
-import { useSnapshot } from 'valtio';
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { createRoom, startRound } from "@/lib/rooms";
+import { useSongsOfRound } from "@/lib/songs";
+import { SpotifyTrackDetail } from "@/lib/spotify";
+import { state } from "@/lib/state";
+import { PauseIcon, PlayIcon, XIcon } from "lucide-react";
+import { FC, useEffect, useRef, useState } from "react";
+import { useSnapshot } from "valtio";
+import Image from "next/image";
 
 export default function Page() {
-  const [topic, setTopic] = useState('');
+  const [topic, setTopic] = useState("");
   const { topic: submittedTopic, room } = useSnapshot(state);
 
   const songs = useSongsOfRound(room, submittedTopic);
 
-  console.log('Songs:', songs);
+  console.log("Songs:", songs);
 
   const handleCreateRoomClick = async () => {
     const roomCode = await createRoom();
@@ -42,7 +45,7 @@ export default function Page() {
       return;
     }
 
-    console.log('Starting new round with topic:', topic);
+    console.log("Starting new round with topic:", topic);
 
     startRound(room!, topic);
     setTopic(topic);
@@ -112,10 +115,13 @@ export default function Page() {
               {submittedTopic}&quot;.
             </p>
             <ul>
-              {songs.map((song) => (
-                <li key={song.songId}>
-                  {song.name} by {song.artist}
-                </li>
+              {songs.map((song, index) => (
+                <SongPreview
+                  key={song.songId}
+                  song={song}
+                  index={index}
+                  user="leon"
+                />
               ))}
             </ul>
           </CardContent>
@@ -124,3 +130,95 @@ export default function Page() {
     </section>
   );
 }
+
+type SongPreviewProps = {
+  song: SpotifyTrackDetail;
+  index: number;
+  user: string;
+};
+
+const SongPreview: FC<SongPreviewProps> = ({ song, index, user }) => {
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    const handlePlay = () => setIsPlaying(true);
+    const handlePause = () => setIsPlaying(false);
+
+    const audioElement = audioRef.current;
+    if (audioElement) {
+      audioElement.addEventListener("play", handlePlay);
+      audioElement.addEventListener("pause", handlePause);
+    }
+
+    return () => {
+      if (audioElement) {
+        audioElement.removeEventListener("play", handlePlay);
+        audioElement.removeEventListener("pause", handlePause);
+      }
+    };
+  }, []);
+
+  const handlePlayPause = () => {
+    if (audioRef.current) {
+      if (isPlaying) {
+        audioRef.current.pause();
+      } else {
+        audioRef.current.play();
+      }
+    }
+  };
+
+  const isPlayable = song.previewUrl != "";
+
+  return (
+    <li key={index} className={`p-2 border-b`}>
+      <div className="flex items-center space-x-4">
+        <Button
+          asChild
+          variant="ghost"
+          onClick={handlePlayPause}
+          className="p-0"
+        >
+          <div className="relative">
+            <Image
+              src={song.thumbnailUrl}
+              alt={`${song.name} thumbnail`}
+              width={48}
+              height={48}
+              className="object-cover rounded-full"
+            />
+            <div
+              className={`absolute bg-black bg-opacity-60 rounded-full w-12 h-12 flex items-center justify-center ${
+                isPlayable ? "hidden" : ""
+              }`}
+            >
+              <XIcon className="w-8 h-8 text-white" />
+            </div>
+            <div
+              className={`absolute inset-0 rounded-full flex items-center justify-center ${
+                !isPlayable ? "hidden" : ""
+              }`}
+            >
+              {!isPlaying ? (
+                <PlayIcon className="w-8 h-8 text-white" />
+              ) : (
+                <PauseIcon className="w-8 h-8 text-white" />
+              )}
+            </div>
+          </div>
+        </Button>
+        <div className="space-y-2">
+          <p className="truncate w-[500px] text-xl font-semibold">
+            {song.name}
+          </p>
+          <p className="truncate w-[450px]">{song.artist}</p>
+        </div>
+        <audio ref={audioRef} controls className="hidden">
+          <source src={song.previewUrl} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
+      </div>
+    </li>
+  );
+};
