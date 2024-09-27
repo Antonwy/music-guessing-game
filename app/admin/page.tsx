e4677b0d343593cd58f2b465a91e96451c251c72
Leon Staufer <leon@staufer.me>
2024-09-27 16:37:38 +0200

    add admin logic

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -9,17 +9,25 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 import { createRoom } from "@/lib/rooms";
 import { useState } from "react";
 
 export default function Page() {
   const [roomCode, setRoomCode] = useState<string | undefined>(undefined);
+  const [topic, setTopic] = useState("");
 
   const handleCreateRoomClick = async () => {
     const roomCode = await createRoom();
 
     setRoomCode(roomCode);
   };
+
+  const handleTopicChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTopic(event.target.value);
+  };
+
+  const handleNewRoundClick = async () => {};
 
   return (
     <section className="p-4 flex gap-2 flex-col">
@@ -49,13 +57,35 @@ export default function Page() {
             and clicking "Begin round".
           </p>
           <p>
+            Topics could be anything from "Main Character Vibes" to "Love Song"
+            to "Looking out the window during a train ride". Be creative!
+          </p>
+          <p>
             Be careful this will end the current round and all already submitted
             songs.
           </p>
+          <Input
+            placeholder="Topic for the songs"
+            value={topic}
+            onChange={handleTopicChange}
+          />
         </CardContent>
         <CardFooter>
-          <Button onClick={handleCreateRoomClick}>Begin round</Button>
+          <Button onClick={handleNewRoundClick}>Begin round "{topic}"</Button>
         </CardFooter>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Submitted Songs for Round "{topic}"</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>
+            Below you will find a list of submitted songs for the topic "{topic}
+            "".
+          </p>
+          <ul>{}</ul>
+        </CardContent>
       </Card>
     </section>
   );
