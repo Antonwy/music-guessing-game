991371542cadee284eb7c3162bd1a701320d8770
Leon Staufer <leon@staufer.me>
2024-09-27 16:29:39 +0200

    refactor suggestions

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,16 @@
-'use client';
+"use client";
 
-import { Button } from '@/components/ui/button';
-import { createRoom } from '@/lib/rooms';
-import { useState } from 'react';
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { createRoom } from "@/lib/rooms";
+import { useState } from "react";
 
 export default function Page() {
   const [roomCode, setRoomCode] = useState<string | undefined>(undefined);
@@ -15,9 +23,40 @@ export default function Page() {
 
   return (
     <section className="p-4 flex gap-2 flex-col">
-      <h1 className="text-lg font-bold">Room Creation</h1>
-      <Button onClick={handleCreateRoomClick}>Create Room</Button>
-      {roomCode && <div>Room Code: {roomCode}</div>}
+      <Card>
+        <CardHeader>
+          <CardTitle>Create a room</CardTitle>
+        </CardHeader>
+        <CardContent>
+          Create a new room for the music guessing game and share this room code
+          with friends.
+          {roomCode && <div>Room Code: {roomCode}</div>}
+        </CardContent>
+        <CardFooter>
+          <Button onClick={handleCreateRoomClick} disabled={!!roomCode}>
+            Create Room
+          </Button>
+        </CardFooter>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Begin the next round</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>
+            Start the next round of the game by entering a new topic for music
+            and clicking "Begin round".
+          </p>
+          <p>
+            Be careful this will end the current round and all already submitted
+            songs.
+          </p>
+        </CardContent>
+        <CardFooter>
+          <Button onClick={handleCreateRoomClick}>Begin round</Button>
+        </CardFooter>
+      </Card>
     </section>
   );
 }
